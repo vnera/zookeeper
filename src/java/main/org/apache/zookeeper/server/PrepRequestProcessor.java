07e68c34e1e2430d84c2bfc5e2a4d0ffac659ab2
Patrick Hunt <phunt@cloudera.com>
2013-12-04 17:18:21 -0800

    PATCH-221. ZOOKEEPER-442 - need a way to remove watches that are no longer of interest

--- src/java/main/org/apache/zookeeper/server/PrepRequestProcessor.java
+++ src/java/main/org/apache/zookeeper/server/PrepRequestProcessor.java
@@ -633,6 +633,7 @@ public class PrepRequestProcessor extends Thread implements RequestProcessor {
             case OpCode.getChildren2:
             case OpCode.ping:
             case OpCode.setWatches:
+            case OpCode.removeWatches:
                 zks.sessionTracker.checkSession(request.sessionId,
                         request.getOwner());
                 break;
